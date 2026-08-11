f2a2a6f2bce3a3bdfa505c6fc1e888bc2f936bbf
ALLAN BARBOSA DE QUEIROZ <54579886860@info.local>
2026-08-11 10:42:36 -0300

    :lipstick: Criando a const color

---
+++ FinControl/src/const/const.ts
@@ -0,0 +1,12 @@
+export const colors = {
+    bgc: "#051424",
+    purpleEmphasis: "#A078FF",
+    superface: "#273647",
+    exitRed: "#93000A",
+    transitionRed: "#FFB4AB",
+    transitionGreen: "#4EDEA3",
+    colorFontTile: "#D4E4FA",
+    colorFont: "#CBC3D7",
+    gray: "#494454",
+    white: "#FFFFFF",
+}
