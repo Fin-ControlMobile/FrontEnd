dde83a7a9eeceb52fe82518ded2643631aef27c5
ALLAN BARBOSA DE QUEIROZ <54579886860@info.local>
2026-08-12 09:02:27 -0300

    :sparkles: Criação do tsx e estilização do footer

--- FinControl/src/const/const.ts
+++ FinControl/src/const/const.ts
@@ -1,12 +1,13 @@
 export const colors = {
-    bgc: "#051424",
+    bgc: "#061626",
     purpleEmphasis: "#A078FF",
-    superface: "#273647",
+    purple: "#340080",
+    superface: "#132333",
     exitRed: "#93000A",
     transitionRed: "#FFB4AB",
     transitionGreen: "#4EDEA3",
     colorFontTile: "#D4E4FA",
     colorFont: "#CBC3D7",
-    gray: "#494454",
+    gray: "#958EA0",
     white: "#FFFFFF",
 }
