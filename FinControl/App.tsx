977782314381e938c7b2259656a3e3433f994fa8
GUILHERME RIBEIRO DOS SANTOS <46336142825@info.local>
2026-08-13 10:02:58 -0300

    🎨 style: finalizacao styles

--- FinControl/App.tsx
+++ FinControl/App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Footer from './src/component/footer/footer';
+import { Manrope_800ExtraBold, Manrope_700Bold, Manrope_600SemiBold } from '@expo-google-fonts/manrope';
+import Seguranca from './src/app/seguranca';
+import { useFonts } from 'expo-font';
+import { JetBrainsMono_400Regular } from '@expo-google-fonts/jetbrains-mono';
 export default function App() {
+
+  const [loaded] = useFonts({
+    JetBrainsMono_400Regular,
+    Manrope_600SemiBold,
+    Manrope_700Bold,
+    Manrope_800ExtraBold
+  })
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Footer />
+      <Seguranca />
       <StatusBar style="auto" />
     </View>
   );
